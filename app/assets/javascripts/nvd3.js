22f5a977205336c0b77087c091b60f116d698073
Pascal Weiland <pasweiland@gmail.com>
2015-07-14 12:34:25 +0200

    Updating for first rails test

--- app/assets/javascripts/nvd3.js
+++ app/assets/javascripts/nvd3.js
@@ -1,9 +1,24 @@
-(function() {
+window.nvd3 = (function() {
     'use strict';
 
+    var FORMAT = {
+        DATE: function(d) {return d3.time.format('%x')(new Date(d));},
+        TWO_STEPS: d3.format(',.2f')
+    };
+
+    var TYPE = {
+        AREA_CHART: 0,
+        LINE_CHART: 1
+    };
+
+    var defaultOptions = {
+        type: TYPE.AREA_CHART,
+        xAxis: FORMAT.DATE,
+        yAxis: FORMAT.TWO_STEPS
+    };
+
     function createGraph(options) {
-        return nv.addGraph(createChart);
-        function createChart() {
+        var createChart = function createChart() {
             var chart = nv.models.stackedAreaChart()
                 .margin({right: 100})
                 .x(function(d) { return d[0] })   //We can modify the data accessor functions...
@@ -14,20 +29,14 @@
                 .showControls(true)       //Allow user to choose 'Stacked', 'Stream', 'Expanded' mode.
                 .clipEdge(true);
 
-            //Format x-axis labels with custom function.
-            chart.xAxis
-                .tickFormat(function(d) {
-                    return d3.time.format('%x')(new Date(d))
-                });
+            if (options.xAxis) chart.xAxis.tickFormat(options.xAxis);
+            if (options.yAxis) chart.yAxis.tickFormat(options.yAxis);
 
-            chart.yAxis
-                .tickFormat(d3.format(',.2f'));
-
-            initd3(options.selector, options.data, chart)
+            initd3(options.selector, options.data, chart);
 
             return chart;
-        }
-        function initd3(selector, data, chart) {
+        };
+        var initd3 = function initd3(selector, data, chart) {
             var detached;
             if (!selector) {
                 detached = document.createElement('div');
@@ -39,19 +48,20 @@
                 .call(chart);
 
             nv.utils.windowResize(chart.update);
-        }
+        };
+        return nv.addGraph(createChart);
     }
 
     function create(options) {
-        options = options || {};
+        options = merge(options, defaultOptions);
         options.context = options.context || document.currentScript.parentNode;
         if(options.url) {
             d3.json(options.url, function(data) {
                 options.data = data;
-                createGraph(options)
+                createGraph(options);
             });
         } else if (options.data) {
-            createGraph(options)
+            createGraph(options);
         }
     }
 
@@ -59,5 +69,42 @@
     return {
         create: create
     };
+
+    // (private) helpers
+    function isArray(variable) {
+        return Object.prototype.toString.call(variable) === "[object Array]";
+    }
+
+    function isFunction(variable) {
+        return variable instanceof Function;
+    }
+
+    function isPlainObject(variable) {
+        return !isFunction(variable) && variable instanceof Object;
+    }
+
+    function extend(target, source) {
+        var key;
+        for (key in source) {
+            if (isPlainObject(source[key]) || isArray(source[key])) {
+                if (isPlainObject(source[key]) && !isPlainObject(target[key])) {
+                    target[key] = {};
+                }
+                if (isArray(source[key]) && !isArray(target[key])) {
+                    target[key] = [];
+                }
+                extend(target[key], source[key]);
+            } else if (source[key] !== undefined) {
+                target[key] = source[key];
+            }
+        }
+    }
+
+    function merge(obj1, obj2) {
+        var target = {};
+        extend(target, obj1);
+        extend(target, obj2);
+        return target;
+    }
 
 })();
--- lib/nvd3/helper.rb
+++ lib/nvd3/helper.rb
@@ -3,32 +3,36 @@ require "erb"
 
 module Nvd3
   module Helper
-    def line_chart(data_source, options = {})
-      nvd3_chart "LineChart", data_source, options
-    end
+    # def line_chart(data_source, options = {})
+    #   nvd3_chart "LineChart", data_source, options
+    # end
+    #
+    # def pie_chart(data_source, options = {})
+    #   nvd3_chart "PieChart", data_source, options
+    # end
+    #
+    # def column_chart(data_source, options = {})
+    #   nvd3_chart "ColumnChart", data_source, options
+    # end
+    #
+    # def bar_chart(data_source, options = {})
+    #   nvd3_chart "BarChart", data_source, options
+    # end
+    #
+    # def area_chart(data_source, options = {})
+    #   nvd3_chart "AreaChart", data_source, options
+    # end
+    #
+    # def geo_chart(data_source, options = {})
+    #   nvd3_chart "GeoChart", data_source, options
+    # end
+    #
+    # def timeline(data_source, options = {})
+    #   nvd3_chart "Timeline", data_source, options
+    # end
 
-    def pie_chart(data_source, options = {})
-      nvd3_chart "PieChart", data_source, options
-    end
-
-    def column_chart(data_source, options = {})
-      nvd3_chart "ColumnChart", data_source, options
-    end
-
-    def bar_chart(data_source, options = {})
-      nvd3_chart "BarChart", data_source, options
-    end
-
-    def area_chart(data_source, options = {})
+    def areachart(data_source, options = {})
       nvd3_chart "AreaChart", data_source, options
-    end
-
-    def geo_chart(data_source, options = {})
-      nvd3_chart "GeoChart", data_source, options
-    end
-
-    def timeline(data_source, options = {})
-      nvd3_chart "Timeline", data_source, options
     end
 
     private
@@ -36,16 +40,26 @@ module Nvd3
     def nvd3_chart(klass, data_source, options)
       @chartkick_chart_id ||= 0
       options = nvd3_deep_merge(Nvd3.options, options)
-      element_id = options.delete(:id) || "chart-#{@chartkick_chart_id += 1}"
+      element_id = options.delete(:id) || "d3chart-#{@chartkick_chart_id += 1}"
       height = options.delete(:height) || "300px"
       # content_for: nil must override default
       content_for = options.key?(:content_for) ? options.delete(:content_for) : Nvd3.content_for
 
-      html = (options.delete(:html) || %(<div id="%{id}" style="height: %{height}; text-align: center; color: #999; line-height: %{height}; font-size: 14px; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helvetica, sans-serif;">Loading...</div>)) % {id: ERB::Util.html_escape(element_id), height: ERB::Util.html_escape(height)}
+      html = (options.delete(:html) || %(<div id="%{id}" style="height: %{height}; >Loading nvd3...</div>)) % {id: ERB::Util.html_escape(element_id), height: ERB::Util.html_escape(height)}
+
+      # data_source = data_source.respond_to?(:chart_json) ? data_source.chart_json : data_source.to_json
+      data_source = [{
+                         key: 'testname',
+                         values: data_source.to_a
+                     }]
 
       js = <<JS
 <script type="text/javascript">
-  new Chartkick.#{klass}(#{element_id.to_json}, #{data_source.respond_to?(:chart_json) ? data_source.chart_json : data_source.to_json}, #{options.to_json});
+  nvd3.create({
+    name: #{element_id.to_json},
+    data: #{data_source},
+    options: #{options.to_json}
+  });
 </script>
 JS
       if content_for
