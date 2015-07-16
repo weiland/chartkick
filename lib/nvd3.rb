206de77a884152f55d86a8bfc866f1318fb5b4e1
Pascal Weiland <pasweiland@gmail.com>
2015-07-16 16:16:42 +0200

    Remove chart_json

--- lib/nvd3.rb
+++ lib/nvd3.rb
@@ -11,16 +11,3 @@ module Nvd3
   self.options = {}
 end
 
-# for multiple series
-# use Enumerable so it can be called on arrays
-module Enumerable
-  def chart_json
-    if is_a?(Hash) && (key = keys.first) && key.is_a?(Array) && key.size == 2
-      group_by { |k, _v| k[0] }.map do |name, data|
-        {name: name, data: data.map { |k, v| [k[1], v] }}
-      end
-    else
-      self
-    end.to_json
-  end
-end
